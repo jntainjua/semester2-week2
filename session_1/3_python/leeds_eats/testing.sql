cbbe91e1d04a95a7dbaa310c527b5810ab835341
Amy <117829330+Scsabr@users.noreply.github.com>
2026-01-28 12:20:19 +0000

    Added base to sql file

---
+++ session_1/3_python/leeds_eats/testing.sql
@@ -0,0 +1,11 @@
+-- Enable readable output format
+.mode columns
+.headers on
+
+-- Instructions for students:
+-- 1. Open SQLite in terminal: sqlite3 food_delivery.db
+-- 2. Load this script: .read testing.sql
+-- 3. Exit SQLite: .exit
+
+
+-- write your sql code here
